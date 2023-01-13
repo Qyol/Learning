893e1277f42a5e27b40d421fd18fb21b5e6e1d66
Georgios Ioannou <geoioannou@athtech.gr>
2023-01-13 19:23:13 +0200

    complete

---
+++ EnhancedIfStatements/EnhancedIfStatements/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace EnhancedIfStatements
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int inputTemperature = 0;
+            string temperatureMessage = string.Empty;
+            string inputValue = string.Empty;
+
+            Console.WriteLine("Enter the current temperature : ");
+            inputValue = Console.ReadLine();
+
+            bool validInteger = int.TryParse(inputValue, out inputTemperature);
+
+            if(validInteger)
+            {
+                temperatureMessage = inputTemperature <= 15 ? "It is too cold here" : (inputTemperature >= 16 && inputTemperature <= 28) ? "It is ok here" : inputTemperature > 28 ? "It is hot here" : "";
+                Console.WriteLine(temperatureMessage);
+            }else
+            {
+                Console.WriteLine("Not a valid temperature");
+            }
+        }
+
+        
+    }
+}
