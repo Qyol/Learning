7435a50933772f163f56b611d0f783f71184cfb5
Georgios Ioannou <geoioannou@athtech.gr>
2023-01-13 18:35:55 +0200

    complete

---
+++ IfStatements2/IfStatements2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace IfStatements2
+{
+    internal class Program
+    {
+        static int highscore = 300;
+        static string highscorePlayer = "George";
+        static void Main(string[] args)
+        {
+            Check(250, "Dimitris");
+            Check(315, "Alex");
+            Check(350, "George");
+        }
+        public static void Check(int score, string playerName)
+        {
+            if (score > highscore)
+            {
+                highscore= score;
+                highscorePlayer= playerName;
+
+                Console.WriteLine("New highscore is " + score);
+                Console.WriteLine("It is now held by " + playerName);
+            }else
+            {
+                Console.WriteLine("The old highscore could not be broken. It is still " + highscore + " and held by " + highscorePlayer);
+            }
+        }
+    }
+}
